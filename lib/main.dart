38c5e294aaccfccae6c4e4e72b252e123ee683cb
Ilgiz Safiulin <isafiulin00@gmail.com>
2026-03-04 16:50:17 +0600

    change url strategy for github pages

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:my_portfolio/app.dart';
 import 'package:my_portfolio/core/constants/global_general_constants.dart';
 
 void configureApp() {
-  usePathUrlStrategy();
+  setUrlStrategy(const HashUrlStrategy());
 }
 
 void main() async {
@@ -15,15 +15,18 @@ void main() async {
   await EasyLocalization.ensureInitialized();
   configureApp();
 
-  runApp(ScreenUtilInit(
-    designSize: const Size(430, 932),
-    minTextAdapt: true,
-    splitScreenMode: true,
-    fontSizeResolver: FontSizeResolvers.height,
-    child: EasyLocalization(
+  runApp(
+    ScreenUtilInit(
+      designSize: const Size(430, 932),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      fontSizeResolver: FontSizeResolvers.height,
+      child: EasyLocalization(
         supportedLocales: const [Locale('en'), Locale('ru')],
         path: GlobalGeneralConstants.translationsPath,
         fallbackLocale: const Locale('ru'),
-        child: const ProviderScope(child: MyApp())),
-  ));
+        child: const ProviderScope(child: MyApp()),
+      ),
+    ),
+  );
 }
